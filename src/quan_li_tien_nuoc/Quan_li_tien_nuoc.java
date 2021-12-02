f848c62a82a6c4280effabfcfef1e2defa1bd411
4robed <kidzzzz14@gmail.com>
2021-12-02 15:52:01 +0700

    First commit

---
+++ src/quan_li_tien_nuoc/Quan_li_tien_nuoc.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quan_li_tien_nuoc;
+
+/**
+ *
+ * @author kidzz
+ */
+public class Quan_li_tien_nuoc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
